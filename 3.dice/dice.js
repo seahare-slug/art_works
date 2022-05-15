fb72d3a7df83ca7649ff8d6aa3d0002c6a819d65
kimsongsub <busanbak2000@naver.com>
2022-05-15 17:11:39 +0900

    Update dice.js
    make "make dice function"

--- 3.dice/dice.js
+++ 3.dice/dice.js
@@ -1,5 +1,19 @@
-const example = document.querySelector("#example-element");
-const stopBtn = document.querySelector("button");
+let numberOfDice = 1;
+
+const imagesSource = [
+  "./img/dog.jpg",
+  "./img/aaron.jpg",
+  "./img/fox.jpg",
+  "./img/GusDapperton_FirstAid.jpg",
+  "./img/luckydaye.jpg",
+  "./img/poem.jpg",
+  "./img/saanvi-vavilala.jpg",
+  "./img/whale.jpg",
+];
+const diceFace = ["front", "back", "right", "left", "top", "bottom"];
+const stopBtn = document.querySelector("#stop-btn");
+const makeBtn = document.querySelector("#make-btn");
+const example = document.querySelector(".example-element");
 
 let i = 0;
 let animationFrame;
@@ -25,7 +39,7 @@ function handleAnimationFrame() {
 
 stopBtn.addEventListener("click", handleAnimationFrame);
 
-let diceObj = function (faceLocation, imgLocation) {
+let diceObj = function (faceLocation, imgLocation, appendChildLocation) {
   const newDiv = document.createElement("div");
   const newImg = document.createElement("img");
   newDiv.appendChild(newImg);
@@ -33,7 +47,33 @@ let diceObj = function (faceLocation, imgLocation) {
   imgSrc.value = imgLocation;
   newImg.setAttributeNode(imgSrc);
   newDiv.classList.add(faceLocation, "face");
-  example.appendChild(newDiv);
+  appendChildLocation.appendChild(newDiv);
 };
 
-diceObj("front", "./img/dog.jpg");
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+}
+
+function makeNewDice() {
+  const newWrap = document.createElement("div");
+  numberOfDice += 1;
+  // newWrap.id = `wrap${numberOfDice}`;
+  newWrap.style.marginTop = `${getRandomIntInclusive(1, 90)}vh`;
+  newWrap.style.marginLeft = `${getRandomIntInclusive(1, 90)}vw`;
+  const newExample = document.createElement("section");
+  newWrap.appendChild(newExample);
+  newExample.classList.add("example-element");
+  const appendChildLocation = newExample;
+
+  diceFace.forEach((face) => {
+    diceObj(
+      face,
+      imagesSource[getRandomIntInclusive(0, 5)],
+      appendChildLocation
+    );
+  });
+}
+
+makeBtn.addEventListener("click", makeNewDice);

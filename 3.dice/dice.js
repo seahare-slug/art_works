9ab3bd66f1713af00e97128323a45dc8dc708045
kimsongsub <busanbak2000@naver.com>
2022-05-16 22:07:13 +0900

    Update dice.js
    make class Dice()

--- 3.dice/dice.js
+++ 3.dice/dice.js
@@ -1,7 +1,4 @@
-let numberOfDice = 1;
-
 const exampleElementArray = [document.querySelector(".dice")];
-
 const imagesSource = [
   "./img/dog.jpg",
   "./img/aaron.jpg",
@@ -17,6 +14,7 @@ const stopBtn = document.querySelector("#stop-btn");
 const makeBtn = document.querySelector("#make-btn");
 
 let i = 0;
+let numberOfDice = 1;
 let animationFrame;
 
 rotateDice();
@@ -41,9 +39,9 @@ function handleAnimationFrame() {
 
 stopBtn.addEventListener("click", handleAnimationFrame);
 
-let diceObj = function (faceLocation, imgLocation, appendChildLocation) {
+let diceObj = function (faceLocation, imgLocation) {
   const newDiv = document.createElement("div");
-  appendChildLocation.appendChild(newDiv);
+  document.querySelector(`#wrap${numberOfDice} > section`).appendChild(newDiv);
   const newImg = document.createElement("img");
   newDiv.appendChild(newImg);
   const imgSrc = document.createAttribute("src");
@@ -58,29 +56,57 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function makeNewDice() {
-  const newWrap = document.createElement("div");
-  document.body.appendChild(newWrap);
-  numberOfDice += 1;
-  newWrap.id = `wrap${numberOfDice}`;
-  newWrap.style.position = "absolute";
-  newWrap.style.top = `${getRandomIntInclusive(10, 90)}vh`;
-  newWrap.style.left = `${getRandomIntInclusive(10, 90)}vw`;
-  const newExample = document.createElement("section");
-  newWrap.appendChild(newExample);
-  newExample.classList.add("dice");
-  exampleElementArray.push(newExample);
-  const appendChildLocation = document.querySelector(
-    `#wrap${numberOfDice} > section`
-  );
+// function makeDice() {
+//   const newWrap = document.createElement("div");
+//   document.body.appendChild(newWrap);
+//   numberOfDice += 1;
+//   newWrap.id = `wrap${numberOfDice}`;
+//   newWrap.style.position = "absolute";
+//   newWrap.style.top = `${getRandomIntInclusive(10, 70)}vh`;
+//   newWrap.style.left = `${getRandomIntInclusive(10, 50)}vw`;
+//   const newExample = document.createElement("section");
+//   newWrap.appendChild(newExample);
+//   newExample.classList.add("dice");
+//   exampleElementArray.push(newExample);
 
-  diceFace.forEach((face) => {
-    diceObj(
-      face,
-      imagesSource[getRandomIntInclusive(0, 5)],
-      appendChildLocation
-    );
-  });
+//   diceFace.forEach((face) => {
+//     diceObj(
+//       face,
+//       imagesSource[getRandomIntInclusive(0, imagesSource.length - 1)]
+//     );
+//   });
+// }
+
+class Dice {
+  constructor() {
+    this.top = `${getRandomIntInclusive(10, 70)}vh`;
+    this.left = `${getRandomIntInclusive(10, 50)}vw`;
+  }
+  makeDice() {
+    const newWrap = document.createElement("div");
+    document.body.appendChild(newWrap);
+    numberOfDice += 1;
+    newWrap.id = `wrap${numberOfDice}`;
+    newWrap.style.position = "absolute";
+    newWrap.style.top = this.top;
+    newWrap.style.left = this.left;
+    const newExample = document.createElement("section");
+    newWrap.appendChild(newExample);
+    newExample.classList.add("dice");
+    exampleElementArray.push(newExample);
+
+    diceFace.forEach((face) => {
+      diceObj(
+        face,
+        imagesSource[getRandomIntInclusive(0, imagesSource.length - 1)]
+      );
+    });
+  }
 }
 
-makeBtn.addEventListener("click", makeNewDice);
+function handleMakeDice() {
+  const dice = new Dice();
+  dice.makeDice();
+}
+
+makeBtn.addEventListener("click", handleMakeDice);
